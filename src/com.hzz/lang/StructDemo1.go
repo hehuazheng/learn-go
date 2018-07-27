4e17ca330b3bf1f38cce3d6343dc08f5d2352cef
hejiafu <hejiafu@secoo.com>
2018-07-27 17:14:30 +0800

    add

---
+++ src/com.hzz/lang/StructDemo1.go
@@ -0,0 +1,12 @@
+package main
+
+// A is a struct
+type A struct {
+	a int
+}
+
+// B is a struct
+type B struct {
+	b int
+	A
+}
---
+++ src/com.hzz/lang/StructDemo1_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_initB(t *testing.T) {
+	var b = B{b: 2, A: A{a: 3}}
+	fmt.Println("b value ", b.A.a, b.b)
+}
